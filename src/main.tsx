156d4576c40a1c71188ccd6efdd501628a970806
oops-lobster <131652676+oops-lobster@users.noreply.github.com>
2026-08-21 15:09:11 +0900

    Apply updated Day 1 arrival time

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './data/day1Update'
+import './data/day1FlightUpdate'
 import App from './App'
 import './styles.css'
 
