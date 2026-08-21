8b0b7ce9e734736f7b356632b892695cd84e8fcd
oops-lobster <131652676+oops-lobster@users.noreply.github.com>
2026-08-21 16:55:51 +0900

    Apply latest Day 1 flight and cafe updates

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './data/day1Update'
+import './data/day1FlightUpdate'
+import './data/day1LatestUpdate'
 import App from './App'
 import { WeatherProvider } from './components/WeatherProvider'
 import './styles.css'
