1a7748a95c56d8c760b2ceb2791bf71085add2b9
oops-lobster <131652676+oops-lobster@users.noreply.github.com>
2026-08-21 15:09:04 +0900

    Update Day 1 Taipei arrival time

---
+++ src/data/day1FlightUpdate.ts
@@ -0,0 +1,8 @@
+import { days } from './trip'
+
+const dayOne = days.find((day) => day.id === 'day-1')
+const taoyuanArrival = dayOne?.schedule.find((item) => item.title === '타오위안공항 도착')
+
+if (taoyuanArrival) {
+  taoyuanArrival.time = '08:50'
+}
