8eb8f91ebfc5027c019ae9adfb3e54fa2cdd8e92
oops-lobster <131652676+oops-lobster@users.noreply.github.com>
2026-08-23 05:44:34 +0900

    Fix Day 1 arrival time to 09:50

--- src/data/day1FlightUpdate.ts
+++ src/data/day1FlightUpdate.ts
@@ -4,5 +4,5 @@ const dayOne = days.find((day) => day.id === 'day-1')
 const taoyuanArrival = dayOne?.schedule.find((item) => item.title === '타오위안공항 도착')
 
 if (taoyuanArrival) {
-  taoyuanArrival.time = '08:50'
+  taoyuanArrival.time = '09:50'
 }
